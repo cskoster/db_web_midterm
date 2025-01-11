a7e36386791db0cb06aaf790574369757f85b6e4
Christopher Koster <chris.koster@gmail.com>
2025-01-11 11:53:00 +0200

    removed cruft

--- routes/attendee.js
+++ routes/attendee.js
@@ -9,26 +9,6 @@
 const express = require("express");
 const router = express.Router();
 
-// // Attendee Event page
-// router.get("/event", function (req, res) {
-//   res.render("attendeeEvent.ejs")
-// });
-
-
-// Attendee Home
-// router.get("/", function (req, res) {
-//   let query = "SELECT title, description, heading FROM site_settings where name='attendee_page'";
-//   // Execute the query and render the page with the results
-//   global.db.all(query,
-//     function (err, result) {
-//       if (err) {
-//         next(err); //send the error on to the error handler
-//       } else {
-//         res.render("attendeeHome.ejs", result[0])
-//       }
-//     }
-//   );
-// });
 
 // Attendee Home
 router.get("/", function (req, res) {
