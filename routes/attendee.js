e49565a266ac4c056920a2e6ef6f98376f607c55
Christopher Koster <chris.koster@gmail.com>
2025-01-12 14:38:20 +0200

    got almost all the info for a booking

--- routes/attendee.js
+++ routes/attendee.js
@@ -56,6 +56,7 @@ router.get('/event/:event_id', function (req, res) {
 
   // there is a ':' in front. TODO: why?
   let event_id = req.params.event_id[1];
+  data.event_id = event_id;
 
   let query = "SELECT title, description, heading FROM site_settings where name='attendee_events_page'";
   // Execute the query and render the page with the results
@@ -81,7 +82,7 @@ router.get('/event/:event_id', function (req, res) {
               next(err); //send the error on to the error handler
             } else {
               data.event = result[0];
-              console.log(data);
+              console.log("Event data:", data);
 
 
               res.render("attendeeEvent.ejs", data);
@@ -108,13 +109,13 @@ router.post("/make_booking", (req, res, next) => {
 
   // let dateBooked = new Date();
   // dateBooked = util.formatDate(dateBooked);
-  console.log(req.body.name, req.body.email, req.body.num_tickets);
+  // console.log(req.body.name, req.body.email, req.body.num_tickets);
 
   data.name = req.body.name;
   data.email = req.body.email;
   data.num_tickets = req.body.num_tickets;
+  data.event_id = req.body.event_id;
   let userName = req.body.name;
-
   let query = "SELECT * FROM email_accounts INNER JOIN users ON email_accounts.user_id = users.user_id WHERE user_name=?;";
   let query_parameters = [userName];
 
@@ -145,7 +146,7 @@ router.post("/make_booking", (req, res, next) => {
 
 
                 let query = "SELECT user_id from users where user_name=?";
-                console.log("USERNAME: ", userName);
+                // console.log("USERNAME: ", userName);
                 let query_parameters = [userName];
 
                 global.db.all(query, query_parameters,
@@ -156,9 +157,24 @@ router.post("/make_booking", (req, res, next) => {
                       next(err); //send the error on to the error handler
                     } else {
 
-                      data.nameLookup = result; // only one page with this name. db constraint
-                      console.log("HERE: ", data);
+                      data.nameLookup = result[0]; // only one page with this name. db constraint
+                      // console.log("ccccc: ", data);
                       //res.render("attendeeBooked", data);
+
+                      // if we are here the name and id are in data.nameLookup
+
+                      // GOT THE event_id, the user_id
+                      // console.log("HERE: ", data);
+                      console.log("user_id: ", data.nameLookup.user_id,
+                        " user email : ", data.email,
+                        " event_id: ", data.event_id,
+                        " number_tickets: ", data.num_tickets,
+                        " date booked: ")
+
+
+
+
+
                     }
                   }
                 );
@@ -167,9 +183,10 @@ router.post("/make_booking", (req, res, next) => {
           );
         } // done: INSERT into users
 
-        // if we are here the name and id are in data.nameLookup
 
-        // GOT THE FUCKING ID!
+
+
+
 
 
 
@@ -183,7 +200,7 @@ router.post("/make_booking", (req, res, next) => {
             } else {
 
               data.page = result[0]; // only one page with this name. db constraint
-              console.log(data);
+              //console.log(data);
               res.render("attendeeBooked", data);
             }
           }
